2c58acbf38c88fb2e6aa031a7cf6f5b32936d9a4
lpp18 <agcu1842@126.com>
2020-02-06 20:50:56 +0800

    init

---
+++ server.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2020/2/6.
+ */
+let http = require('http');
+let server = http.createServer(function(req, res){
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/html');
+    res.end('hello blog')
+})
+server.listen(3000, ()=>{
+    console.log('server is listening at point 3000')
+})
